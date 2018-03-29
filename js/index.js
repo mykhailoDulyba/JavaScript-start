da257606dcc99cf4567b1d89799d4e4839a077a2
softservemike <mykhailodulyba@gmail.com>
2018-03-29 13:13:42 +0000

    done

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+
+const result = solveQuard(a, b, c);
+document.write('a ='+a +'<br>', 'b ='+b +'<br>', 'c ='+c);
+document.write('<br>'+result);
+
+function solveQuard(a, b, c){
+     const d = calcDisc(a, b, c);
+      let x1;
+      let x2;
+    if( d < 0 ){
+        return 'Фу таким бути';
+    }else if (d === 0){
+        x1 = (-b+Math.sqrt(d))/(2*a);
+        x2 = (-b-Math.sqrt(d))/(2*a);
+        return 'x1 =' + x1 +'x2 =' + x2;
+    }else {
+        x1 = (-b+Math.sqrt(d))/(2*a);
+        x2 = (-b-Math.sqrt(d))/(2*a);
+        return 'x1 =' + x1 +'<br>' +'x2 =' + x2;
+    }
+}
+
+function calcDisc(a, b, c){
+    return b*b-4*a*c;
+}
